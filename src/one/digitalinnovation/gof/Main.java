a724e32ebdc5992c79ad2609e2c314c5ad2ac7b5
guisilvapedrosa <101533436+guisilvapedrosa@users.noreply.github.com>
2022-08-08 05:05:27 -0300

    Add files via upload

---
+++ src/one/digitalinnovation/gof/Main.java
@@ -0,0 +1,50 @@
+package one.digitalinnovation.gof;
+
+import one.digitalinnovation.gof.facade.Facade;
+import one.digitalinnovation.gof.singleton.SingletonEager;
+import one.digitalinnovation.gof.singleton.SingletonLazy;
+import one.digitalinnovation.gof.singleton.SingletonLazyHolder;
+import one.digitalinnovation.gof.strategy.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Singleton
+        SingletonLazy lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+        lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+
+        SingletonEager eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+        eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+
+        SingletonLazyHolder holder = SingletonLazyHolder.getInstancia();
+        System.out.println(holder);
+        holder = SingletonLazyHolder.getInstancia();
+        System.out.println(holder);
+
+        //Strategy
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento agessivo = new ComportamentoAgessivo();
+
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();
+        robo.mover();
+        robo.setComportamento(defensivo);
+        robo.mover();
+        robo.setComportamento(agessivo);
+        robo.mover();
+        robo.mover();
+        robo.mover();
+
+        //Facade
+
+        Facade facade = new Facade();
+        facade.migrarCliente("Klinger", "69096000");
+
+    }
+}
